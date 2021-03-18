fbbdff415f5345973fd00ba5cada292c31d32420
HugoRig <logicsystem28@gmail.com>
2021-03-18 10:26:45 +0100

    maj classes

--- app/src/main/java/com/example/dainim/controller/SignoutListener.java
+++ app/src/main/java/com/example/dainim/controller/SignoutListener.java
@@ -4,14 +4,14 @@ import android.view.View;
 
 import com.example.dainim.view.ProfilActivity;
 
-public class updateListener implements View.OnClickListener {
+public class SignoutListener implements View.OnClickListener {
     private ProfilActivity profilActivity;
 
-    public updateListener(ProfilActivity profilActivity){
+    public SignoutListener(ProfilActivity profilActivity){
         this.profilActivity = profilActivity;
     }
     @Override
     public void onClick(View v) {
-        profilActivity.updateUsernameInFirebase();
+        profilActivity.signOutUserFromFirebase();
     }
 }
